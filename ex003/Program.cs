36cea5c0b369f5d7d625312fd8c96e30e3be336b
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-20 15:37:48 +0300

    Add example 003 as an answer on task 6.3

---
+++ ex003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Проверка на полиндром
+
+
+public class Task3
+{
+public static void Main(string[] args)
+{
+
+string input = "шалаш";
+
+bool isPalindrome = IsPalindrome(input);
+
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+public static bool IsPalindrome(string str){
+string normalized = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+return normalized.SequenceEqual(normalized.Reverse());
+}
+}
+
