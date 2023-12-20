7f8a8586355ee1304be2bab36a1684331df9b4cb
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-20 15:37:15 +0300

    Add example 001 as an answer on task 6.1

---
+++ ex001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+string GetStringFromCharArray(char [,] chars){
+    string res = "";
+    foreach (char elem in chars){ // в каждом элементе масссива мы будем что-то делать, еще один вида функции цикла
+        res += elem;
+    } 
+    return res;
+}
+
+char [,] chars = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+string str = GetStringFromCharArray(chars);
+Console.WriteLine(str);
+
+
+
+// string GetStringFromCharArray(char [] chars){
+//     string res = "";
+//     foreach (char elem in chars){ // в каждом элементе масссива мы будем что-то делать, еще один вида функции цикла
+//         res += elem;
+//     } 
+//     return res;
+// }
+
+// char [] chars = {'a', 'b', 'c', 'd'};
+// string str = GetStringFromCharArray(chars);
+// Console.WriteLine(str);
